791df6b1638fc9a44c16b0a04422cf34c185cc99
idontgetoutmuch <idontgetoutmuch@gmail.com>
2019-10-02 12:36:38 +0000

    Make bug reveal itself

--- bugReport.nix
+++ bugReport.nix
@@ -8,27 +8,27 @@ let
             rev = "a7b685352330ebf7d8794aed64663a9ee92dcdab";
           }) {}
         );
-        accelerate-llvm = super.haskell.lib.dontCheck (hself.callPackage /home/sundials/accelerate-llvm/accelerate-llvm { });
-        # accelerate-llvm = super.haskell.lib.dontCheck (
-        #   hself.callCabal2nix "accelerate-llvm" (builtins.fetchGit {
-        #     url = "https://github.com/AccelerateHS/accelerate-llvm";
-        #     rev = "1680d3fdb34073d2cc25c265968a695525bc1bf2";
-        #   }) { subpath = "accelerate-llvm"; }
-        # );
-        accelerate-llvm-ptx = super.haskell.lib.dontCheck (hself.callPackage /home/sundials/accelerate-llvm/accelerate-llvm-ptx { });
-        # accelerate-llvm-ptx = super.haskell.lib.dontCheck (
-        #   hself.callCabal2nix "accelerate-llvm-ptx" (builtins.fetchGit {
-        #     url = "https://github.com/AccelerateHS/accelerate-llvm";
-        #     rev = "1680d3fdb34073d2cc25c265968a695525bc1bf2";
-        #   }) { subpath = "accelerate-llvm-ptx"; }
-        # );
-        accelerate-llvm-native = super.haskell.lib.dontCheck (hself.callPackage /home/sundials/accelerate-llvm/accelerate-llvm-native { });
-        # accelerate-llvm-native = super.haskell.lib.dontCheck (
-        #   hself.callCabal2nix "accelerate-llvm-native" (builtins.fetchGit {
-        #     url = "https://github.com/AccelerateHS/accelerate-llvm";
-        #     rev = "1680d3fdb34073d2cc25c265968a695525bc1bf2";
-        #   }) { subpath = "accelerate-llvm-native"; }
-        # );
+        # accelerate-llvm = super.haskell.lib.dontCheck (hself.callPackage /home/sundials/accelerate-llvm/accelerate-llvm { });
+        accelerate-llvm = super.haskell.lib.dontCheck (
+          hself.callCabal2nix "accelerate-llvm" (builtins.fetchGit {
+            url = "https://github.com/AccelerateHS/accelerate-llvm";
+            rev = "1680d3fdb34073d2cc25c265968a695525bc1bf2";
+          }) { subpath = "accelerate-llvm"; }
+        );
+        # accelerate-llvm-ptx = super.haskell.lib.dontCheck (hself.callPackage /home/sundials/accelerate-llvm/accelerate-llvm-ptx { });
+        accelerate-llvm-ptx = super.haskell.lib.dontCheck (
+          hself.callCabal2nix "accelerate-llvm-ptx" (builtins.fetchGit {
+            url = "https://github.com/AccelerateHS/accelerate-llvm";
+            rev = "1680d3fdb34073d2cc25c265968a695525bc1bf2";
+          }) { subpath = "accelerate-llvm-ptx"; }
+        );
+        # accelerate-llvm-native = super.haskell.lib.dontCheck (hself.callPackage /home/sundials/accelerate-llvm/accelerate-llvm-native { });
+        accelerate-llvm-native = super.haskell.lib.dontCheck (
+          hself.callCabal2nix "accelerate-llvm-native" (builtins.fetchGit {
+            url = "https://github.com/AccelerateHS/accelerate-llvm";
+            rev = "1680d3fdb34073d2cc25c265968a695525bc1bf2";
+          }) { subpath = "accelerate-llvm-native"; }
+        );
         accelerate-fft = super.haskell.lib.dontCheck (
           hself.callCabal2nix "accelerate-fft" (builtins.fetchGit {
             url = "https://github.com/AccelerateHS/accelerate-fft";
